b8935b691eb36b9dcc15f9fb554c78aaf452951b
Vishal P <vishalpsj99@gmail.com>
2024-12-22 17:34:20 +0530

    added favicon, greetings added along with clock, style.css modiified, .gitattributes added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 let curTime = document.getElementById('curTime')
+let greetings = document.getElementById('greetings')
 
 setInterval(() => {
     curTime.innerText = new Date().toLocaleTimeString()
 }, 1000);
+
+
+
+const makegreetings = (greet) =>{
+    greetings.innerText = greet
+} 
+
+let hour = new Date().getHours()
+
+
+if(hour < 12){
+    makegreetings("Good Morning 🌞🌄")
+}
+else if(hour < 16){
+    makegreetings("Good Afternoon 🌞")
+}
+else if(hour < 20){
+    makegreetings("Good Evening 🌇")
+}
+else{
+    makegreetings("Good Night 🌛")
+}
